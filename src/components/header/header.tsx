e87e459e543ec07443a7406fad12eb7765445db6
Brandon🤖 <brandon@effortlessdevelopment.com>
2022-12-05 19:22:53 -0500

    new changes to submit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,29 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, useStylesScoped$, useStore } from '@builder.io/qwik';
 import { QwikLogo } from '../icons/qwik';
-import styles from './header.css?inline';
 
 export default component$(() => {
-  useStylesScoped$(styles);
+  
+  // QWIK state for menu toggle
+  const state = useStore({ 
+    menuOpen: false 
+  });
+
 
   return (
     <header>
-      <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank">
-          <QwikLogo />
+      <div className="mb-4 mt-8 flex justify-between items-center px-4">
+        <a href="https://brandongottshall.com/"  target="_blank" className='h-full'>
+          <h1 className="text-xs font-semibold text-red-600">
+            Brandon Gottshall
+          </h1>
         </a>
+        <div className='w-10 -mb-2'>
+              <button onClick$={() => state.menuOpen = true}className='flex-col w-10 h-full space-y-2'>
+                <div className={`w-10 h-1 bg-black transform-gpu rotate-0 duration-1000 ${state.menuOpen ? 'rotate-45' : ''}`} />
+                <div className={`w-10 h-1 bg-black transform-gpu rotate-0 duration-1000 ${state.menuOpen ? '-translate-y-3 -rotate-45' : ''}`} />
+                <div className={`w-10 h-1 bg-black transition-opacity duration-500 opacity-100 ${state.menuOpen ? 'opacity-0' : ''}`} />
+              </button>
+            </div>
       </div>
     </header>
   );
